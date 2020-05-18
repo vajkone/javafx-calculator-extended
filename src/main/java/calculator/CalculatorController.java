11d70afb6e90c48aa22252abb500e75d78a00d41
Vajk Garbóczy <vajkone18@gmail.com>
2020-05-18 19:12:30 +0200

    Added clearEverything(), makeFraction() and changeSign() functions to their corresponding buttons.

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -5,6 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.apache.commons.lang3.StringUtils;
+
 
 public class CalculatorController {
 
@@ -15,6 +17,8 @@ public class CalculatorController {
     private boolean startNumber = true;
     private double number1;
     private String operator = "";
+
+
 
     @FXML
     private void initialize() {
@@ -34,6 +38,28 @@ public class CalculatorController {
     }
 
     @FXML
+    public void clearEverything(){
+        display.setText("0");
+    }
+
+    @FXML
+    public void makeFraction(){ display.setText(display.getText() + "."); }
+
+    @FXML
+    public void changeSign(){
+
+        if (display.getText().startsWith("-")){
+            display.setText(StringUtils.remove(display.getText(),'-'));
+        }else{
+            display.setText("-"+display.getText());
+        }
+
+    }
+
+
+
+
+    @FXML
     public void processOperator(ActionEvent event) {
         String operatorPressed = ((Button) event.getSource()).getText();
         System.out.println(operatorPressed);
@@ -43,16 +69,27 @@ public class CalculatorController {
            }
            double number2 = Double.parseDouble(display.getText());
            double result = calculator.calculate(number1, number2, operator);
-           display.setText(String.format("%.0f", result));
+           if (result%1==0){
+               display.setText(String.format("%.0f", result));
+           }else{
+               String format = StringUtils.strip(String.format("%.10f", result),"0").replace(',','.');
+               if (format.startsWith(".")){
+                   display.setText("0"+format);
+               }else{
+                   display.setText(format);
+               }
+           }
+
            operator = "";
+            System.out.println(result);
         } else {
             if (! operator.isEmpty()) {
                 return;
             }
             number1 = Double.parseDouble(display.getText());
             operator = operatorPressed;
-            startNumber = true;
         }
+        startNumber=true;
     }
 
 }
